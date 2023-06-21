3f8fc2e5091fdc42409f40712dad268e7751cb8a
jijiji1299 <wl3330384@naver.com>
2023-06-22 04:54:23 +0900

    글 읽기 프론트, 댓글달기 기능 수정

--- Melting/src/main/java/com/melting/controller/ReplyController.java
+++ Melting/src/main/java/com/melting/controller/ReplyController.java
@@ -1,19 +1,29 @@
 package com.melting.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.melting.domain.Board;
+import com.melting.domain.Member;
 import com.melting.domain.Reply;
+import com.melting.service.MemberService;
 import com.melting.service.ReplyService;
 
 @Controller
 public class ReplyController {
+	private MemberService memberService;
 	
 	@Autowired
 	ReplyService replyService;
+	
+	public ReplyController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
 	
 	/*댓글 달기*/
 	@PostMapping("/reply/replywrite")
@@ -25,9 +35,17 @@ public class ReplyController {
 	
 	/*댓글 삭제*/
 	@GetMapping("/reply/replydelete")
-	public String replydelete(int replyseq) {
+	public String replydelete(int replyseq, Model model, Authentication authentication) {
 		int result = replyService.deleteReply(replyseq);
-		System.out.println(result);
+		
+		// 유저이름 불러오기 (membername)
+		if (authentication != null) {
+			String username = authentication.getName();
+			Member member = memberService.getMemberUsername(username);
+			String membername = member.getMembername();
+			model.addAttribute("membername", membername);
+		}
+		
 		return "redirect:/";
 	}
 	
